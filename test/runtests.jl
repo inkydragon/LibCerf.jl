d8555e69935978e2bb81c000c988e102b38b7b8c
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-22 20:36:26 +0800

    test: add special value tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,14 +10,31 @@ for T in (Float64, Float32, Float16)
 
     @testset "erf($CT)" begin
         @test isnan(erf(CT(NaN)))
+
+        @test erf(CT(Inf)) == CT(1)
+        @test erf(CT(10)) == CT(1)
+        @test erf(zero(CT)) == zero(CT)
+        @test erf(-CT(10)) == -CT(1)
+        @test erf(-CT(Inf)) == -CT(1)
     end
 
     @testset "erfc($CT)" begin
         @test isnan(erfc(CT(NaN)))
+
+        @test erfc(CT(Inf)) == CT(0)
+        @test erfc(CT(100)) == CT(0)
+        @test erfc(zero(CT)) == CT(1)
+        @test erfc(-CT(10)) == CT(2)
+        @test erfc(-CT(100)) == CT(2)
+        @test erfc(-CT(Inf)) == CT(2)
     end
 
     @testset "faddeeva_w($CT)" begin
         @test isnan(faddeeva_w(CT(NaN)))
+
+        @test faddeeva_w(CT(Inf)) == zero(CT)
+        @test faddeeva_w(zero(CT)) == one(CT)
+        @test faddeeva_w(-CT(Inf)) == -zero(CT)
     end
 
     @testset "im_w($T)" begin
@@ -27,6 +44,9 @@ for T in (Float64, Float32, Float16)
         # @test isnan(im_w(T(NaN), T(0.0)))
         @test_broken im_w(T(0.0), T(NaN)) === T(NaN)
         @test_broken im_w(T(NaN), T(0.0)) === T(NaN)
+
+        @test im_w(T(0)) == T(0)
+        @test im_w(T(0), T(0)) == T(0)
     end
 
     @testset "re_w($T)" begin
@@ -34,6 +54,10 @@ for T in (Float64, Float32, Float16)
         @test isnan(re_w(T(0.0), T(NaN)))
         # @test isnan(re_w(T(NaN), T(0.0)))
         @test_broken re_w(T(NaN), T(0.0)) === T(NaN)
+
+        @test re_w(T(100), T(0)) == T(0)
+        @test re_w(T(0), T(0)) == T(1)
+        @test re_w(-T(100), T(0)) == T(0)
     end
 
     @testset "voigt($T)" begin
@@ -45,25 +69,43 @@ for T in (Float64, Float32, Float16)
         @test_broken voigt(T(NaN), T(0.0), T(0.0)) === T(NaN)
         @test isnan(voigt(T(0.0), T(NaN), T(0.0)))
         @test isnan(voigt(T(0.0), T(0.0), T(NaN)))
+
+        @test isfinite(voigt(T(0), one(T), one(T)))
+        @test iszero(voigt(T(Inf), one(T), one(T)))
     end
 
     @testset "voigt_hwhm($T)" begin
         @test isnan(voigt_hwhm(T(NaN), T(NaN)))
         @test isnan(voigt_hwhm(T(0.0), T(NaN)))
         @test isnan(voigt_hwhm(T(NaN), T(0.0)))
+
+        @test voigt_hwhm(T(0), one(T)) == one(T)
     end
 
     for TT in (T, CT)
         @testset "erfcx($TT)" begin
             @test isnan(erfcx(TT(NaN)))
+
+            @test erfcx(zero(CT)) == one(CT)
+            @test iszero(erfcx(CT(Inf)))
+            @test isinf(erfcx(CT(-Inf)))
         end
 
         @testset "erfi($TT)" begin
             @test isnan(erfi(TT(NaN)))
+
+            @test erfi(CT(Inf)) == CT(Inf)
+            @test erfi(zero(CT)) == zero(CT)
+            @test erfi(-CT(Inf)) == -CT(Inf)
         end
 
         @testset "dawson($TT)" begin
             @test isnan(dawson(TT(NaN)))
+
+            @test iszero(dawson(CT(Inf)))
+            @test dawson(zero(CT)) == zero(CT)
+            @test iszero(dawson(CT(-Inf)))
         end
     end # TT
+
 end # T
